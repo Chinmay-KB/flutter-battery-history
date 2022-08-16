28533b05a9825856479979c4cddcf53fd8ae8c31
Chinmay Kabi <chinmay.kabi@live.com>
2022-08-16 16:34:57 +0530

    removed method channel, use event channel

--- android/src/main/kotlin/com/example/battery_history/BatteryHistoryPlugin.kt
+++ android/src/main/kotlin/com/example/battery_history/BatteryHistoryPlugin.kt
@@ -3,33 +3,27 @@ package com.example.battery_history
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.StreamHandler
 
-/** BatteryHistoryPlugin */
-class BatteryHistoryPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
 
+class BatteryHistoryPlugin: FlutterPlugin, StreamHandler {
+
+  private lateinit var channel : EventChannel
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "battery_history")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    channel = EventChannel(flutterPluginBinding.binaryMessenger, "battery_history")
+    channel.setStreamHandler(this)
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    channel.setStreamHandler(null)
+  }
+
+  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun onCancel(arguments: Any?) {
+    TODO("Not yet implemented")
   }
 }
--- lib/battery_history.dart
+++ lib/battery_history.dart
@@ -6,19 +6,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_isolate/flutter_isolate.dart';
 
 class BatteryHistory {
-  static const MethodChannel _channel = MethodChannel('battery_history');
-
   FlutterIsolate? isolate;
   Future<void> startTick() async {
-    // final String? version = await _channel.invokeMethod('getPlatformVersion');
-    // return version;
     final _pref = await SharedPreferences.getInstance();
     _pref.setStringList('time', []);
     Timer.periodic(Duration(seconds: 2), (Timer t) async {
       _pref.setStringList(
           'time', [..._pref.getStringList('time') ?? [], t.tick.toString()]);
-      // _pref.setStringList(
-      //     'time', [..._pref.getStringList('time') ?? [], t.tick.toString()]);
     });
   }
 
